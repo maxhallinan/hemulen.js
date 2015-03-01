9c455f78adb46e698572199e569c88930d30e0b4
Maximum Hallinan <maximumhallinan@gmail.com>
2015-02-28 19:12:54 -0500

    Create Hemulen class

--- src/js/hemulen_new-2.js
+++ src/js/hemulen_new-2.js
@@ -1,20 +1,51 @@
 //binds function Baz to the global namespace
 (function(){
+    var filesStored     = {},
+        usedHashes      = [],
+        formSubmitted   = false; 
+
+    //EVENT EMITTER CLASS
     function EventEmmiter(){}
 
-    EventEmmiter.prototype.helloWorld = function(){console.log('hello world');};
+    EventEmmiter.prototype.helloWorld = function(){console.log(this.namespace);};
 
+    //HEMULEN CLASS
     function Hemulen(element, options){
-        console.log('element: ', element);
-        console.log('options: ', options);
+        this.namespace      = '';
+        this.dropInput      = '';
+        this.fileInput      = '';
+        this.acceptTypes    = [];
+        this.fileMaxSize    = 0;
+        this.fileLimit      = 0;
+        this.beforeSub      = function(){};
+        this.onSubFail      = function(){};
+        this.onSubSuccess   = function(){};
+
+        function extend(options){
+            for (var key in options) {
+                if(options[key].constructor === Object) {
+                    extend.call(this[key], options[key])
+                } else {
+                    if(this.hasOwnProperty(key)) {
+                        this[key] = options[key];
+                    }
+                }
+            }
+        }
+
+        if (options) {extend.call(this, options);}
+ 
     }
 
+    //Hemulen inherits from EventEmitter
     Hemulen.prototype = Object.create(EventEmmiter.prototype);
-
     Hemulen.prototype.constructor = Hemulen;
 
-    Hemulen.prototype.goodbyeWorld = function(){console.log('goodbye world')};
-    
+    //HEMULEN METHODS
+    Hemulen.prototype.goodbyeWorld = function(){console.log(this.namespace)};
+
+
+    //EXPORT HEMULEN
     if (typeof module !== "undefined" && module !== null) {
         module.exports = Hemulen;
     } else {
@@ -23,6 +54,6 @@
 
 })();
 
-var baz = new Hemulen('body', {'hello':'world'});
+var baz = new Hemulen('body', {'namespace':'test'});
 baz.helloWorld();
 baz.goodbyeWorld();
